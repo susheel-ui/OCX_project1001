1889d67256f0ee1fd873664329e5783379a20cdb
developershailendra <85412261+developershailendra@users.noreply.github.com>
2025-10-07 23:45:53 +0530

    something

--- src/main/java/com/example/oceanx/controller/OtpController.java
+++ src/main/java/com/example/oceanx/controller/OtpController.java
@@ -4,6 +4,7 @@ package com.example.oceanx.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +19,18 @@ public class OtpController {
     private OtpService otpService;
 
     @PostMapping("/send")
-    public ResponseEntity<String> sendOtp(@RequestParam String phoneNumber) {
+    public ResponseEntity<String> sendOtp(@RequestBody String phoneNumber) {
     System.out.println("inside controller - sendOtp");
     return otpService.sendOtp(phoneNumber);
     }
 
 
-    // @PostMapping("/verify")
-    // public ResponseEntity<String> verifyOtp(@RequestParam String phoneNumber, @RequestParam String otp) {
-    //     return otpService.verifyOtp(phoneNumber, otp);
-    // }
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyOtp(@RequestBody String phoneNumber, @RequestBody String otp) {
+        return otpService.verifyOtp(phoneNumber, otp);
+    }
 
+    // @PostMapping('/register')
+    // public ResponseEntity<String> registerUser(@RequestParam String phoneNumber, @RequestParam String otp) {
+    //     return otpService.registerUser(phoneNumber, otp);
     }
